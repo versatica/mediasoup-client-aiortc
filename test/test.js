262444a7825ae98fa3226f455d911cb0f3f5d3e5
Iñaki Baz Castillo <ibc@aliax.net>
2020-02-25 15:23:54 +0100

    more logs

--- test/test.js
+++ test/test.js
@@ -13,11 +13,19 @@ test('reproduce aiortc issue 301', async () =>
 	const videoTrack = stream.getTracks()[1];
 
 	// This will invoke processNotification("player.stopTrack") in worker.py
+	// for the player.audio track
 	audioTrack.stop();
 
 	await new Promise((resolve) => setTimeout(resolve, 50));
 
-	// This will invoke processNotification("player.close") in worker.py
+	// This will invoke in worker.py:
+	//
+	// 1. processNotification("player.stopTrack") for player.video track
+	// 2. processNotification("player.close") that will call stop() in
+	//    both player.audio and player.video
+	//
+	// It does not invoke "player.stopTrack" for player.audio track because it
+	// was already stopped (above in audioTrack.stop())
 	stream.close();
 
 	// This will invoke processRequest("dump") in worker.py
@@ -29,4 +37,4 @@ test('reproduce aiortc issue 301', async () =>
 	// This never happens because we get frozen in the previous await
 	// await worker.dump()
 	worker.close();
-}, 4000);
+}, 2000);
