5eadd911dbcf806c3b998ac051e37be7ad4bcfa4
Iñaki Baz Castillo <ibc@aliax.net>
2023-12-19 16:30:54 +0100

    3.9.0

--- worker/setup.py
+++ worker/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mediasoup-client-aiortc",
-    version="3.9.0-pre1",
+    version="3.9.0",
     description="mediasoup-client handler for aiortc Python library",
     url="https://github.com/versatica/mediasoup-client-aiortc",
     author="José Luis Millán Villegas, Iñaki Baz Castillo",
